0b16d89742195e27072a3ca6504833e908e631b5
moobarrac <wahabajibola01@gmail.com>
2022-09-25 22:17:40 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './component/Header';
+import AddPage from './component/AddPage';
+import Watched from './component/Watched';
+import Watchlist from './component/Watchlist';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path='/' exact element={<Watchlist/>}/>
+          <Route path='/watched' exact element={<Watched/>}/>
+          <Route path='/add' exact element={<AddPage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
