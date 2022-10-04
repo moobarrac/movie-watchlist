6cf87c3a62bad68a6aab280f78a630577fc8c8e8
moobarrac <wahabajibola01@gmail.com>
2022-10-04 07:58:08 +0100

    watchlist comp

--- src/component/Watchlist.js
+++ src/component/Watchlist.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import MovieCard from './MovieCard'
 
 const Watchlist = () => {
+  const {watchlist} = useContext(GlobalContext)
   return (
-    <div>Watchlist</div>
+    <div className='movie-page'>
+      <div className='container'>
+        <div className='header'>
+          <h1 className='heading'>My Watchlist</h1>
+          <span className='count-pill'>
+            {watchlist.length} {watchlist.length === 1 ? 'Movie' : 'Movies'}
+          </span>
+        </div>
+        <div className='movie-grid'>
+          {
+            watchlist.length > 0 ? watchlist.map(movie => {
+              return <MovieCard movie={movie} type='watchlist'/>
+            }) : 
+            <div className='no-movies'>No movie in your list, add some!</div>
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
