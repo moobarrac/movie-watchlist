4ec85fcb7e9d2766c1d739f55669125ddfd7c204
moobarrac <wahabajibola01@gmail.com>
2022-10-05 21:12:58 +0100

    get movies from api

--- src/component/AddPage.js
+++ src/component/AddPage.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import ResultCard from './ResultCard'
 
 const AddPage = () => {
+  const API_KEY = '8c0a105c23c82cdb946e4909ea6c5007'
   const [search, setSearch] = useState('')
+  const [results, setResults] = useState([])
+
+  const onChange = e => {
+    e.preventDefault();
+    setSearch(e.target.value)
+  }
+
+  const getMovies = () => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${search}&page=1&include_adult=false`)
+      .then(res => res.json())
+      .then(data => {
+        if(!data.errors) {
+          setResults(data.results)
+        }
+        else {
+          setResults([])
+        }
+      })
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [search])
+
   return (
     <div className='add-page'>
       <div className='container'>
@@ -11,9 +37,19 @@ const AddPage = () => {
               type="text"
               placeholder='Enter movie name'
               value={search}
-              onChange={(e) => setSearch(e.target.value)}
+              onChange={onChange}
             />
           </div>
+          {results.length > 0 && (
+            <ul className='results'>
+              {results.map((movie) => (
+                <li key={movie.id}>
+                  <ResultCard movie={movie}/>
+                </li>
+                )
+              )}
+            </ul>
+          )}
         </div>
       </div>
     </div>
