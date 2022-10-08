edc37c258985835b351c4f92bbfd36f631c718d5
moobarrac <wahabajibola01@gmail.com>
2022-10-08 21:26:22 +0100

    result comp

---
+++ src/component/ResultCard.js
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+const ResultCard = ({movie}) => {
+  const {addMovieToWatchlist, watchlist, addMovieToWatched, watched} = useContext(GlobalContext)
+
+  let storedMovie = watchlist.find(item => item.id === movie.id)
+  let storedWatched = watched.find(item => item.id === movie.id)
+  
+  let WatchListDisabled = storedMovie ? true : storedWatched ? true : false;
+  let WatchedDisabled = storedWatched ? true : false;
+
+  return (
+    <div className='result-card'>
+      <div className='poster-wrapper'>
+        {movie.poster_path ? (
+          <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={`${movie.title} Poster`}/>
+        ) : (
+          <div className='filler-poster'></div>
+        )}
+      </div>
+      <div className='info'>
+        <div className='header'>
+          <h3 className='title'>{movie.title}</h3>
+          <h4 className='release-date'>
+            {movie.release_date ? movie.release_date.substring(0, 4) : '-'}
+          </h4>
+        </div>
+
+        <div className='controls'>
+          <button 
+            className='btn' 
+            disabled={WatchListDisabled}
+            onClick={() => addMovieToWatchlist(movie)}
+          >Add to WatchList</button>
+          <button 
+            className='btn' 
+            disabled={WatchedDisabled}
+            onClick={() => addMovieToWatched(movie)}
+          >Add to Watched</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ResultCard
