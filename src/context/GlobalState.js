0362dc2a6c36bf8ffa76b4f526cc840ca964ab5e
moobarrac <wahabajibola01@gmail.com>
2022-10-18 09:57:54 +0100

    action func

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -23,9 +23,19 @@ export const GlobalProvider = props => {
   const addMovieToWatchlist = movie => {
     dispatch({type: 'ADD_MOVIE_TO_WATCHLIST', payload: movie})
   }
+  const addWatchedToWatchlist = movie => {
+    dispatch({type: 'MOVE_WATCHED_TO_WATCHLIST', payload: movie})
+  }
+  const addMovieToWatched = movie => {
+    dispatch({type: 'ADD_MOVIE_TO_WATCHED', payload: movie})
+  }
   const removeMovieToWatchlist = id => {
     dispatch({type: 'REMOVE_MOVIE_TO_WATCHLIST', payload: id})
   }
+  const removeMovieFromWatched = id => {
+    dispatch({type: 'REMOVE_MOVIE_TO_WATCHED', payload: id})
+  }
+
 
   return (
     <GlobalContext.Provider
@@ -33,7 +43,10 @@ export const GlobalProvider = props => {
         watched: state.watched,
         watchlist: state.watchlist,
         addMovieToWatchlist,
-        removeMovieToWatchlist
+        removeMovieToWatchlist,
+        addMovieToWatched,
+        removeMovieFromWatched,
+        addWatchedToWatchlist
       }}
     >
       {props.children}
